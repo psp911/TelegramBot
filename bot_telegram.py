daca62b4f54017c150f1f85cd1e771c2d4325398
Serg <psp911@yandex.ru>
2021-12-23 00:30:16 +0300

    sql read and write

--- bot_telegram.py
+++ bot_telegram.py
@@ -4,20 +4,20 @@
 import logging
 from aiogram.utils import executor
 from create_bot import dp
-
-
-
-
+from data_base import sqlite_db
 
 
 
 async def on_startup(_):
-	print ('Бот вышел в онлайн')
+	print('Бот вышел в онлайн')
+	sqlite_db.sql_start()
 
 
-from handlers import client, other
+
+from handlers import client, admin, other
 
 client.register_handlers_client(dp)
+admin.register_handlers_admin(dp)
 other.register_handlers_other(dp)
 
 
--- handlers/client.py
+++ handlers/client.py
@@ -1,6 +1,7 @@
 from aiogram import types, Dispatcher
 from create_bot import dp, bot
 from keyboards import kb_client
+from data_base import sqlite_db
 from aiogram.types import ReplyKeyboardRemove
 
 
@@ -24,7 +25,13 @@ async def pizza_place_command(message : types.Message):
 	await bot.send_message(message.from_user.id, 'Ул.Колбасная 15')
 
 
+#@dp.register_message_handler(commands=['Меню'])
+async def pizza_menu_command(message : types.Message):
+	await sqlite_db.sql_read(message)
+
+
 def register_handlers_client(dp : Dispatcher):
 	dp.register_message_handler(command_start, commands=['start', 'help'])
 	dp.register_message_handler(pizza_open_command, commands=['Режим_работы'])
 	dp.register_message_handler(pizza_place_command, commands=['Расположение'])
+	dp.register_message_handler(pizza_menu_command, commands=['Меню'])
